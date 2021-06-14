453a08039b89e24a9045ed2777b3a5c2b31b1506
kimura <t.kimura@zizo.ne.jp>
2021-06-14 18:45:19 +0900

    add home btn

---
+++ components/LinkHome.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  .c-linkHome
+    .c-linkHome__inner
+      nuxt-link(to="/").c-linkHome__button
+</template>
+
+<script>
+export default {
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+  .c-linkHome {
+    $_size: 50px;
+
+    position: fixed;
+    top: 30px;
+    left: 30px;
+    &__button {
+      display: block;
+      width: $_size;
+      height: $_size;
+      border-radius: 50%;
+      background-color: $color_main;
+    }
+  }
+</style>
--- pages/linewave/index.vue
+++ pages/linewave/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <LinkHome />
     <BackLine />
   </div>
 </template>
